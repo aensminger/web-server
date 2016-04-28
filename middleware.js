f783634227142e42851915e347109147340f7ae8
Alex Ensminger <ensmingeraa@gmail.com>
2016-04-27 21:12:14 -0600

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit');
+		next();
+	},
+	logger: function (req, res, next) {
+		var now=new Date().toString();
+		console.log('Request ('+now+'): '+req.method+' '+req.originalUrl);
+		next();
+	}
+}
