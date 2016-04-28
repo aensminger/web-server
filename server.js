68e72d9fa10e5094cea7e47c4647c4862c65dd33
Alex Ensminger <ensmingeraa@gmail.com>
2016-04-27 21:00:57 -0600

    Initialize repo

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var PORT=3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit');
+		next();
+
+	},
+	logger: function (req, res, next) {
+		var now=new Date().toString();
+		console.log('Request ('+now+'): '+req.method+' '+req.originalUrl);
+		next();
+	}
+};
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+// /about
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us');
+});
+// About Us
+
+
+app.use(express.static(__dirname+'/public'));
+//console.log(__dirname);
+app.listen(PORT, function () {
+	console.log('Express server started on port: '+PORT);
+});
